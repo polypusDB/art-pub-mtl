0d62e429b028952fc5ec0d6f4f498edbf85da60c
Angela <sanangelg@gmail.com>
2019-09-11 16:50:49 -0400

    Ajoute de Forms Artistes et Oueuvres, debut de l entete avec la creation du CSS pour Entete et Footer

---
+++ api/vues/VueNavigation.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+    <html lang="fr">
+    
+    <head>
+        <title>L'art public à Montréal</title>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width">
+        
+        <link rel="stylesheet" href="../../css/var.css" type="text/css" media="screen">
+        <link rel="stylesheet" href="../../css/flex.css" type="text/css" media="screen">
+        <link rel="stylesheet" href="../../css/main.css" type="text/css" media="screen">
+        <link rel="stylesheet" href="../../css/entete-footer.css" type="text/css" media="screen">
+        <!--<script src="../../js/plugins.js"></script>-->
+        <script src="../../js/main.js"></script>
+
+        <!-- Code Fontawesome -->
+        <script src="https://kit.fontawesome.com/178b5a49f1.js"></script>
+    </head>
+    
+    <body>
+        <header class="entete">
+            <a href="" class="logo"><img src="../../img/logoAPM.png" alt="Art Public Montreal"></a>
+            <span class="flexgrow"></span>
+            <nav class="menu">
+                <i class="fas fa-home"></i><a href="index.php">Accueil</a>
+                <i class="fas fa-paint-brush"></i><a href="">Artistes</a>
+                <i class="fas fa-brush"></i><a href="">Oeuvres</a>
+                <!-- <i class="fas fa-map-marker-alt"></i><a href="">Parcours</a> -->
+                <!-- <i class="fas fa-hand-holding-heart"></i><a href="">Partenariat</a> -->
+                <i class="fas fa-spray-can"></i><a href="">À Propos</a>
+                <a href=""><i class="fas fa-language"><a></i>
+                    <ul class="menu-langue">
+                        <li><a href="">Francais</a></li>
+                        <li><a href="">English</a></li>
+                    </ul>
+                <span>
+                    <a class="ico-login" href="#"><i class="fab fa-sign-in-alt"></i></a>
+                </span>
+            </nav>
+            <a href="#" class="menu-burger">
+                <i class="fas fa-bars"></i>
+                <i class="fas fa-times"></i>
+            </a>
+        </header>
+    
+    </body>
+</html>
+    
