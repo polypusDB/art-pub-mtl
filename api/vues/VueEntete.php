18a5663dafe16f1e7b6f91da817d9dd99cf9899f
Angela <sanangelg@gmail.com>
2019-09-12 17:01:39 -0400

    Achevement de la entete avec CSS

--- api/vues/VueEntete.php
+++ api/vues/VueEntete.php
@@ -8,12 +8,14 @@
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width">
         
-        <link rel="stylesheet" href="../css/var.css" type="text/css" media="screen">
-        <link rel="stylesheet" href="../css/flex.css" type="text/css" media="screen">
-        <link rel="stylesheet" href="../css/entete-footer.css" type="text/css" media="screen">
-        <link rel="stylesheet" href="../css/main.css" type="text/css" media="screen">
+        <link rel="stylesheet" href="../../css/var.css" type="text/css" media="screen">
+        <link rel="stylesheet" href="../../css/flex.css" type="text/css" media="screen">
+        <link rel="stylesheet" href="../../css/main.css" type="text/css" media="screen">
+        <link rel="stylesheet" href="../../css/entete-footer.css" type="text/css" media="screen">
         <!--<script src="../../js/plugins.js"></script>-->
-        <script src="../js/main.js"></script>
+        <script src="../../js/main.js"></script>
+        <script src="../../js/menu.js"></script>
+        <script src="../../js/responsive.js"></script>
 
         <!-- Code Fontawesome -->
         <script src="https://kit.fontawesome.com/178b5a49f1.js"></script>
