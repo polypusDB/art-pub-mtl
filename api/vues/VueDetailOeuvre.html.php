705a5d212a238a6e4328ab65b201da1b7b75a312
Angela <sanangelg@gmail.com>
2019-09-16 18:17:04 -0400

    avancement CSS formulaires

---
+++ api/vues/VueDetailOeuvre.html.php
@@ -0,0 +1,31 @@
+
+
+<section class="contenu listeOeuvres">
+	<section class="titreListe">
+		<h1>Oeuvres</h1>
+	</section>
+		<section class="oeuvres flex wrap">
+			<?php
+			//foreach ($aData as $cle => $oeuvre) {
+			//extract($oeuvre);
+			?>
+				<section class="oeuvre carte">
+					<header class="image dummy">
+						<a href=""><img src="../img/placeholder_640_480.jpg" alt="Art Public Montreal"></a>
+					</header>
+					<section class="texte">
+						<h2 class="titre">titre</h2>
+						<h4 class="des-oeuvre">description</h4>
+						<h4 class="dimen-oeuvre">Dimension</h4>
+						<h4 class="cate-oeuvre">Categorie</h4>
+						<h4 class="suppor-oeuvre">Support</h4>
+						<h4 class="endroit-oeuvre">Endroit</h4>
+					</section>
+				</section>
+
+			<?php
+			//}
+			?>
+		</section>
+				
+</section>
