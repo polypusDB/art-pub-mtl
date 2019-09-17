02cd428b5f47cd438fe0f3119d729c4b492974cd
marie <mariecharlotterenou@icloud.com>
2019-09-17 18:13:00 -0400

    footer lien

--- api/vues/VuePied.php
+++ api/vues/VuePied.php
@@ -1,10 +1,10 @@
 </body>    
    <footer>
        <section>
-            <img  src="../img/logo_footer/MMC.png" alt="">
-            <img  src="../img/logo_footer/MTL.png" alt="">
-            <img  src="../img/logo_footer/TM.png" alt="">
-            <img  src="../img/logo_footer/QC.png" alt="">
+           <a href="http://ville.montreal.qc.ca/portal/page?_pageid=5017,15631571&_dad=portal&_schema=PORTAL"><img  src="../img/logo_footer/MMC.png" alt=""></a>
+            <a href="http://ville.montreal.qc.ca"><img  src="../img/logo_footer/MTL.png" alt=""></a>
+            <a href="http://www.mtl.org"><img  src="../img/logo_footer/TM.png" alt=""></a>
+            <a href="https://www.quebec.ca/"><img  src="../img/logo_footer/QC.png" alt=""></a>
        </section>
        <div>
             <p>© Tous droits réservés, Ville de Montréal</p>
