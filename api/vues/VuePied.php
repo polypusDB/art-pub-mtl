922bf0ba85f0854fab929ff3366ab51f8e7fac1c
marie <mariecharlotterenou@icloud.com>
2019-09-17 18:22:39 -0400

    footer réseaux sociaux, lien reste à faire

--- api/vues/VuePied.php
+++ api/vues/VuePied.php
@@ -1,6 +1,12 @@
 </body>    
    <footer>
        <section>
+           <i class="fa fa-facebook" aria-hidden="true"></i>
+           <i class="fa fa-twitter" aria-hidden="true"></i>
+           <i class="fa fa-youtube" aria-hidden="true"></i>
+           <i class="fa fa-instagram" aria-hidden="true"></i>
+       </section>
+       <section>
            <a href="http://ville.montreal.qc.ca/portal/page?_pageid=5017,15631571&_dad=portal&_schema=PORTAL"><img  src="../img/logo_footer/MMC.png" alt=""></a>
             <a href="http://ville.montreal.qc.ca"><img  src="../img/logo_footer/MTL.png" alt=""></a>
             <a href="http://www.mtl.org"><img  src="../img/logo_footer/TM.png" alt=""></a>
