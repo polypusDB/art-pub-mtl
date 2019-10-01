80346e06c1dcc3d90484874d667691c06e83d60f
Sanchez Gutierrez <e1895013@cmaisonneuve.qc.ca>
2019-10-01 17:52:56 -0400

    Modification form Connexion et supprimer le form inscription et avancement modification formajaouterOeuvres

--- api/vues/formAjouterOeuvres.php
+++ api/vues/formAjouterOeuvres.php
@@ -11,15 +11,19 @@
         <form action="" method="POST">
             Titre : <input type="text" name="titre" />
             Dimension (m) : <input type="text" name="dimension" />
-            Material (a,b,c) : <input type="text" name="material" />
-            Technique : <input type="text" name="technique" />
+            Matériel (français) : <input type="text" name="mateFR" />
+            Material (english) : <input type="text" name="mateEN" />
+            Technique (français) : <input type="text" name="techFR" />
+            Technical (english) : <input type="text" name="techEN" />
+            Support (français) : <input type="text" name="suppFR" />
+            Support (english) : <input type="text" name="suppEN" />
             Description (français) :
             <textarea rows="4" cols="50" name="descFR"></textarea>
             Description (english) :
             <textarea rows="4" cols="50" name="descEN"></textarea>
-            Categorie :
+            Catégorie :
             <select name="categorie">
-                <option value="choix">Choisir</option>
+                <option value="choix">Choisir une catégorie</option>
                 <?php 
                     if(!empty($liste)){
                         foreach($liste as $li){
@@ -28,10 +32,14 @@
                     }
                 ?>
             </select>
-             
-            Support:
-            <select name="support">
-                <option value="choix">Choisir</option>
+            Artiste:
+            <div class="chercherForm">
+                <i class="fas fa-search"></i>
+                Chercher une artiste<input type="search" placeholder="Chercher">
+            </div>
+            ou
+            <select name="artiste">
+                <option value="choix">Choisir une artiste</option>
                 <?php 
                     if(!empty($liste)){
                         foreach($liste as $li){
@@ -40,10 +48,14 @@
                     }
                 ?>
             </select>
-             
-            Endroit:
-            <select name="endroit">
-                <option value="choix">Choisir</option>
+            Parc : <input type="text" name="parc" />
+            Batiment : <input type="text" name="batiment" />
+            Adresse : <input type="text" name="adresse" />
+            Latitude : <input type="text" name="latitude" />
+            Longitude : <input type="text" name="longitude" />
+            Arrondisement :
+            <select name="categorie">
+                <option value="choix">Choisir un arrondisement</option>
                 <?php 
                     if(!empty($liste)){
                         foreach($liste as $li){
@@ -52,10 +64,6 @@
                     }
                 ?>
             </select>
-             
-            Description : <input type="text" name="description" /> 
-            Description : <input type="text" name="description" /> 
-
             <!--Télécharger une image-->
             Image : 
             <input type="hidden" name="MAX_FILE_SIZE" value="200000" />
