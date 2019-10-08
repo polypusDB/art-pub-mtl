ddc848b619da5efcbed4027b1b3d91ab4e5ad97c
marie <mariecharlotterenou@icloud.com>
2019-10-08 17:13:50 -0400

    changements place html a propos + création section à la une

--- api/vues/VueHome.php
+++ api/vues/VueHome.php
@@ -8,20 +8,35 @@
             </div>
         </a>
     </div>
-    <section class="apm">
-        <div>
-            <p class="propos">À PROPOS</p>
-            <h1><a href="/art-pub-mtl/api/apropos">Art public montréal</a></h1>
-            <p>Art public Montréal est une initiative de la Ville de Montréal visant à rassembler, au sein d’un partenariat unique, les propriétaires d’œuvres d’art public présentes sur le territoire montréalais et les acteurs du rayonnement de la métropole. En collaboration avec Tourisme Montréal, son objectif est d’accroître la notoriété de Montréal à titre de destination internationale d’art public.</p>
-            <div class="savoir">
-                <a href="/art-pub-mtl/api/apropos">En savoir plus</a>
-                <i class="fas fa-arrow-alt-circle-right"></i>
-            </div>
+<section class="une">
+    <div>
+        <div class="BlocUne">
+        <h1>À la une</h1>
+        <h2>Chaque semaine, un artiste ou une oeuvre est mis en évidence.</h2>
         </div>
-    </section>
+        <section class="oeuvres flex wrap">
+            <div class="oeuvre carte">
+                <div  class="image dummy">
+                <a href=""><img src="../img/placeholder_640_480.jpg" alt="Art Public Montreal"></a>
+                </div>
+                <div class="texte">
+                    <section>
+                    <div>
+                        <h2 class="titre"><a href=""></a> C.A Barbier</h2>
+                        <div class="auteur"><a href="">Muraliste</a></div>
+                    </div>
+                    <p>Art public Montréal est une initiative de la Ville de Montréal visant à rassembler, au sein d’un partenariat unique, les propriétaires d’œuvres d’art public présentes sur le territoire montréalais et les acteurs du rayonnement de la métropole.</p>
+                    <input class="btnArtiste" type="button" href="" value="Découvrir d'avantage">
+                </section>
+				</div>
+            </div>
+        </section>
+    </div>
+    
+</section>
         <section class="artiste">
             <div>
-                <h1><a href="/art-pub-mtl/api/artiste">Artistes</a></h1>
+                <h1><a href="/art-pub-mtl/api/artiste">PARCOURS</a></h1>
                 <div class="listeArtiste">
                     <h2>+ de 850 Oeuvres d’art Public et d’artistes + Phillip ADAMS + SBUONE + Harlan JOHNSON + INTI + Suzan VACHON +  
                     <a href="/art-pub-mtl/api/artiste">plein d’autres encore</a></h2>
@@ -34,4 +49,15 @@
             <h2>Le village imaginé - <br/>PIERRE BOURGAULT</h2>
         </a>
     </div>
+    <section class="apm">
+        <div>
+            <p class="propos">À PROPOS</p>
+            <h1><a href="/art-pub-mtl/api/apropos">Art public montréal</a></h1>
+            <p>Art public Montréal est une initiative de la Ville de Montréal visant à rassembler, au sein d’un partenariat unique, les propriétaires d’œuvres d’art public présentes sur le territoire montréalais et les acteurs du rayonnement de la métropole.</p>
+            <div class="savoir">
+                <a href="/art-pub-mtl/api/apropos">En savoir plus</a>
+                <i class="fas fa-arrow-alt-circle-right"></i>
+            </div>
+        </div>
+    </section>
    
